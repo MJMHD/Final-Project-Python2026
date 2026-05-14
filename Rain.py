f5f964ab900366574a584b00d5097cd6aad20f90
Colepac <colepac17@outlook.com>
2026-05-14 13:32:16 -0400

    Separated files to test the backgrounds better

--- Rain.py
+++ Rain.py
@@ -113,78 +113,24 @@ def update():
     app = ctk.CTk()
     app.geometry("420x500")
     app.title("Weather App")
-    app.configure(fg_color="black")
+    app.configure(fg_color="sky blue")
 
-    #nighttime
-
-    #hour = datetime.now().hour
-    hour = 20
-
-    if hour >= 20 or hour < 5:
-        img = Image.open("moon.png").convert("RGBA")
+    if description == 51 or 61 or 80:
+        img = Image.open("Rain.png").convert("RGBA")
         img = img.resize((230, 216))
 
         photo = ImageTk.PhotoImage(img)
 
-        label = Label(app, image=photo, bg="black", borderwidth=0, highlightthickness=0)
-        label.pack(pady=(40, 0))
+        label = Label(app, image=photo, bg="gray", borderwidth=0, highlightthickness=0)
+        label.pack(pady=(20, 0))
 
-        main_frame = ctk.CTkFrame(app, corner_radius=15, fg_color="black")
+        main_frame = ctk.CTkFrame(app, corner_radius=15, fg_color="gray")
         main_frame.pack(padx=20, pady=20, fill="both", expand=True)
 
         title = ctk.CTkLabel(main_frame, text="Weather", font=("Arial", 28, "bold"), text_color="yellow")
         title.pack(pady=(20, 10))
 
-        search_frame = ctk.CTkFrame(main_frame, fg_color="black")
-        search_frame.pack(pady=10)
-
-        city_entry = ctk.CTkEntry(search_frame, placeholder_text="Enter city...", width=200)
-        city_entry.grid(row=0, column=0, padx=5)
-
-        search_btn = ctk.CTkButton(search_frame, text="Search")
-        search_btn.grid(row=0, column=1, padx=5)
-
-        weather_card = ctk.CTkFrame(main_frame, corner_radius=20, fg_color="blue")
-        weather_card.pack(pady=20, padx=10, fill="both", expand=True)
-
-        city_label = ctk.CTkLabel(weather_card, text=place, font=("Arial", 22), text_color="lightgray")
-        city_label.pack(pady=(20, 5))
-
-        temp_label = ctk.CTkLabel(weather_card, text=(current["temperature"]), font=("Arial", 50, "bold"),
-                                  text_color="lightgray")
-        temp_label.pack()
-
-        condition_label = ctk.CTkLabel(weather_card, text=description, font=("Arial", 18), text_color="lightgray")
-        condition_label.pack(pady=5)
-
-        info_frame = ctk.CTkFrame(weather_card, fg_color="transparent")
-        info_frame.pack(pady=20)
-
-        # feelslike = ctk.CTkLabel(info_frame, text=(feels_like(temp, wind)), justify="center", text_color="lightgray")
-        # feelslike.grid(row=0, column=0, padx=20)
-
-        wind = ctk.CTkLabel(info_frame, text=("Windspeed:", current["windspeed"]), justify="center",
-                            text_color="lightgray")
-        wind.grid(row=0, column=1, padx=20)
-
-    #clear sky
-
-    elif description == 0 or 1:
-        img = Image.open("Sun-Transparent-PNG.png").convert("RGBA")
-        img = img.resize((230, 216))
-
-        photo = ImageTk.PhotoImage(img)
-
-        label = Label(app, image=photo, bg="sky blue", borderwidth=0, highlightthickness=0)
-        label.pack(pady=(20,0))
-
-        main_frame = ctk.CTkFrame(app, corner_radius=15, fg_color="sky blue")
-        main_frame.pack(padx=20, pady=20, fill="both", expand=True)
-
-        title = ctk.CTkLabel(main_frame, text="Weather", font=("Arial", 28, "bold"), text_color="yellow")
-        title.pack(pady=(20, 10))
-
-        search_frame = ctk.CTkFrame(main_frame, fg_color="sky blue")
+        search_frame = ctk.CTkFrame(main_frame, fg_color="gray")
         search_frame.pack(pady=10)
 
         city_entry = ctk.CTkEntry(search_frame, placeholder_text="Enter city...", width=200)
